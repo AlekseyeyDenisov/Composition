15d9ae4b1a755d78f0961e35821c920905192d9c
77den <77denisov@gmail.com>
2022-06-29 14:00:24 +0300

    View Binding

--- app/src/main/java/ru/dw/composition/presentation/ChooseLevelFragment.kt
+++ app/src/main/java/ru/dw/composition/presentation/ChooseLevelFragment.kt
@@ -6,14 +6,25 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import ru.dw.composition.R
+import ru.dw.composition.databinding.FragmentChooseLevelBinding
 
 
 class ChooseLevelFragment : Fragment() {
+    private var _binding: FragmentChooseLevelBinding? = null
+    private val binding: FragmentChooseLevelBinding
+        get() = _binding ?: throw RuntimeException("FragmentChooseLevelBinding == null")
+
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.fragment_choose_level, container, false)
+    ): View {
+        _binding = FragmentChooseLevelBinding.inflate(inflater,container,false)
+        return binding.root
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
     }
 }
--- app/src/main/java/ru/dw/composition/presentation/WelcomeFragment.kt
+++ app/src/main/java/ru/dw/composition/presentation/WelcomeFragment.kt
@@ -1,20 +1,41 @@
 package ru.dw.composition.presentation
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import ru.dw.composition.R
+import androidx.fragment.app.Fragment
+import ru.dw.composition.databinding.FragmentGameBinding
+import ru.dw.composition.databinding.FragmentWelcomeBinding
+import java.lang.RuntimeException
 
 
 class WelcomeFragment : Fragment() {
+    private  var _binding: FragmentWelcomeBinding? = null
+    private val binding : FragmentWelcomeBinding
+    get() = _binding ?: throw RuntimeException("FragmentWelcomeBinding == null")
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.fragment_welcome, container, false)
+    ): View {
+        _binding = FragmentWelcomeBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.buttonUnderstand.setOnClickListener {
+
+        }
+    }
+
+
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
     }
 
 }
